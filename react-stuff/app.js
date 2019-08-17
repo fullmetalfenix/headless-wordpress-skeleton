68e9b781231abdaaef4227e3ea2510f53fbce198
Mr <fullmetalfenix@protonmail.com>
2019-08-17 14:54:52 -0400

    Added ternary for read more
    added ternary for rendering the content body / excerpt on read more

--- react-stuff/app.js
+++ react-stuff/app.js
@@ -1,11 +1,13 @@
 class Initial extends React.Component{
 
+	
+
   constructor(props) {
     super(props);
 
     this.state = {
-      hello: 'true',
-	  posts: [],
+      posts: [],
+	selectedPost: '',
     };
   }
 	
@@ -14,15 +16,25 @@ fetch('http://tryingout.s437.sureserver.com/wp-json/wp/v2/posts')
 	.then(x => x.json())
 	.then(x => this.setState({posts: x}))
 } 
+
+	renderBody(index){
+		this.setState({selectedPost:index});
+	 }	
 	render(){
-	 return(
+
+
+		return(
 	 <div>
 		<h1>Blog.</h1>
-		 {console.log(this.state.posts)}
 			{this.state.posts.map((posts, index) => (
-		 <div className="post-containers"  key={index}>
+		 <div className="post-containers" id={"post-container-" + index}  key={index}>
 			<h2>{posts.title.rendered}</h2>
-			<p>{posts.content.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '')}</p>
+			<p >{
+	(this.state.selectedPost === index ? this.state.posts[index].content.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '') : posts.excerpt.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '').replace(/\[\&hellip\;\]/g, '...'))
+				
+								  
+			}</p>
+			<button onClick={() => { this.renderBody(index) }}>Read More</button>
 		 </div>
 		 )
 	 )}
