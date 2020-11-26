cd59e420a3c40a95e2f0920cea7f4e07bed40bf4
Mr <jpihlk@yahoo.com>
2020-11-26 15:21:21 -0500

    Update app.js
    added comments

--- react-stuff/app.js
+++ react-stuff/app.js
@@ -1,22 +1,20 @@
 class Initial extends React.Component{
-
-	
-
+// initialize state and set posts.
   constructor(props) {
     super(props);
-
     this.state = {
       posts: [],
-	selectedPost: '',
+      selectedPost: '',
     };
   }
-	
+// On component mount (to the dom) fetch the posts	
 componentDidMount(){
-fetch('http://tryingout.s437.sureserver.com/wp-json/wp/v2/posts')
+// Replace '<url-to-your-wp-site>' with your site 	
+fetch('<url-to-your-wp-site>/wp-json/wp/v2/posts')
 	.then(x => x.json())
 	.then(x => this.setState({posts: x}))
 } 
-
+	// this method swaps between the exerpt and the full posts content
 	renderBody(index){
 		this.setState({selectedPost:index});
 	 }	
@@ -30,9 +28,8 @@ fetch('http://tryingout.s437.sureserver.com/wp-json/wp/v2/posts')
 		 <div className="post-containers" id={"post-container-" + index}  key={index}>
 			<h2>{posts.title.rendered}</h2>
 			<p >{
+			{/* Whats this? Regex to take the '<p></p>' tags away from the post body- wordpress wraps the body of the post in this automatically */}
 	(this.state.selectedPost === index ? this.state.posts[index].content.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '') : posts.excerpt.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '').replace(/\[\&hellip\;\]/g, '...'))
-				
-								  
 			}</p>
 			<button onClick={() => { this.renderBody(index) }}>Read More</button>
 		 </div>
