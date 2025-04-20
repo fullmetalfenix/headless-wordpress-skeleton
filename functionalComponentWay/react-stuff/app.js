e46aea805283b8fd56f7f59a0538beb334584ea4
Mr <jpihlk@yahoo.com>
2025-04-20 13:51:46 -0400

    added some styling

--- functionalComponentWay/react-stuff/app.js
+++ functionalComponentWay/react-stuff/app.js
@@ -1,35 +1,91 @@
-const Initial = () =>{
+const FirstPost = ({ post, selectedPost, setSelectedPost }) => (
+  <div className="post-containers" id={"first-container"}>
+    <h2>{post.title.rendered}</h2>
 
-  {/* as this is using a cdn and you cant import, add 'React.' before useState or useEffect */}
-  var [posts, setPosts] = React.useState([])
-  var [selectedPost, setSelectedPost] = React.useState('')
+    <div>
+      {selectedPost === 0 ? (
+        post.content.rendered.replace(/<\/p>/g, "").replace(/<p>/g, "")
+      ) : (
+        <span>
+          {post.excerpt.rendered
+            .replace(/<\/p>/g, "")
+            .replace(/<p>/g, "")
+            .replace(/\[\&hellip\;\]/g, "...")}
+          <button className="readmore" onClick={() => setSelectedPost(0)}>
+            Read More
+          </button>
+        </span>
+      )}
+    </div>
+  </div>
+);
+
+const RemainingPosts = React.lazy(() => {
+  return new Promise(resolve => {
+    const Component = ({ posts, selectedPost, setSelectedPost }) => (
+      <div>
+        {posts.slice(1).map((post, index) => (
+          <div className="post-containers" id={"post-container-" + (index + 1)} key={index + 1}>
+            <h2>{post.title.rendered}</h2>
+            <p>
+              {selectedPost === (index + 1)
+                ? post.content.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '')
+                : post.excerpt.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '').replace(/\[\&hellip\;\]/g, '...')}
+            </p>
+            <button className="readmore" onClick={() => setSelectedPost(index + 1)}>
+              Read More
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+    resolve({ default: Component });
+  });
+});
+
+const Initial = () => {
+  {
+    /* as this is using a cdn and you cant import, add 'React.' before useState or useEffect */
+  }
+  var [posts, setPosts] = React.useState([]);
+  var [selectedPost, setSelectedPost] = React.useState("");
+  const [showMore, setShowMore] = React.useState(false);
 
   React.useEffect(() => {
-    fetch('<your-site>/wp-json/wp/v2/posts')
-        .then(x => x.json())
-        .then(x => setPosts(x))
+    fetch("http://localhost/gatsbywp/wordpress/wp-json/wp/v2/posts")
+      .then((x) => x.json())
+      .then((x) => setPosts(x))
       .catch((err) => {
         alert(err);
-      })
-    }, [])
-  return(
-          <div>
-             <h1>Blog.</h1>
-                 {posts.map((post, index) => (
-                  <div className="post-containers" id={"post-container-" + index}  key={index}>
-                     <h2>{post.title.rendered}</h2>
-                     {/* Whats this? Regex to take the '<p></p>' tags away from the post body- wordpress wraps the body of the post in this automatically 
-                      this is just a demo so Regex is fine in my opinion - otherwise you could checkout html-react-parser (not with a cdn though) or use .split() on it or something but thats up to you                   
-                     */}
-                     <p>{selectedPost === index ? post.content.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '') : post.excerpt.rendered.replace(/<\/p>/g, '').replace(/<p>/g, '').replace(/\[\&hellip\;\]/g, '...')}</p>
-                     {/* This renders the body of the post instead of the excerpt when you select 'read more'*/}
-                     <button className="readmore" onClick={() => {setSelectedPost(index)}}>Read More</button>
-                  </div>
-                ))}
-         </div>
+      });
+  }, []);
+  return (
+    <div className="blog-container">
+      <h1>Blog.</h1>
+      {posts.length > 0 && (
+        <FirstPost
+          post={posts[0]}
+          index={0}
+          selectedPost={selectedPost}
+          setSelectedPost={setSelectedPost}
+        />
+      )}
 
-    )
-}
+      {showMore && posts.length > 1 && (
+        <React.Suspense fallback={<div>Loading more posts...</div>}>
+          <RemainingPosts 
+            posts={posts} 
+            selectedPost={selectedPost} 
+            setSelectedPost={setSelectedPost} 
+          />
+        </React.Suspense>
+      )}
 
+      {!showMore && posts.length > 1 && (
+        <button className="load-more-button" onClick={() => setShowMore(true)}>Load More Posts</button>
+      )}
+    </div>
+  );
+};
 
-    ReactDOM.render(<Initial />, document.getElementById('app-container'));
+ReactDOM.render(<Initial />, document.getElementById("app-container"));
